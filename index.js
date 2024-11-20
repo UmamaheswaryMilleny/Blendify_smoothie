f8763231ff6116b5c7ee584d58926391116d9263
Umamaheswary Milleny <umamaheswarymillini@gmail.com>
2024-11-20 10:49:05 +0530

    login signup done

---
+++ index.js
@@ -0,0 +1,47 @@
+const env = require("dotenv").config()
+const express = require("express")
+const session=require("express-session")
+const app = express();
+const db=require("./config/db")
+const path = require("path")
+const userRouter=require("./routes/userRouter")
+const adminRouter=require('./routes/adminRouter')
+const passport=require("./config/passport")
+db()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(session({
+    secret:process.env.SESSION_SECRET  || 'mySecret',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        secure:false,
+        httpOnly:true,
+        maxAge:72*60*60*1000
+    }
+}))
+
+
+app.use(passport.initialize())
+
+
+app.use((req,res,next)=>{
+    res.set('cache-control','no-store')
+    next()
+})
+
+app.set("view engine","ejs")
+app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.use("/",userRouter)
+// app.use('/admin',adminRouter)
+
+const PORT = 8080 || process.env.PORT
+app.listen(PORT,()=>{
+    console.log('http://localhost:8080')
+})
+
+module.exports=app;
